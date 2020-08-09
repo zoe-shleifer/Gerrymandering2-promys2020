fbf64eec39ea771e7d3aa42242ca9d8c5f4adeea
Duckasaur <zshleifie38@gmail.com>
2020-08-09 02:36:15 -0400

    boom boom

---
+++ gerry_tree/helpers.py
@@ -0,0 +1,78 @@
+def cleanup(DATA_DIR):
+    block_data = pd.read_csv(f'{DATA_DIR}/small_final_attributes.csv')
+    # {block group : {school: distance in seconds from block group to school}}
+    dist_dict = read_file(f'{DATA_DIR}/distance_data.txt')
+    # {school: [block groups in district]}
+    school_dict = read_file(f'{DATA_DIR}/original_districts.txt')
+    schoollist = school_dict.keys()
+
+    # block_data is a table (pandas DataFrame) holding all the attributes of every block group and school (rows are block groups)
+    # dealing with the messiness of pandas read_csv with list/tuple entries
+    block_data['GEOID10'] = block_data['GEOID10'].astype('str')
+    block_data = block_data.set_index('GEOID10')
+    for j in range(len(block_data)):
+        string_list = block_data['borders'].iloc[j].split(',')[1:-1]
+        good_list = []
+        for i in range(len(string_list)):
+            if string_list[i][2:-1] in block_data.index:
+                good_list.append(string_list[i][2:-1])
+        block_data['borders'].iloc[j] = good_list
+    string_list = list(block_data['POS'])
+    for j in range(len(block_data)):
+        good_list = string_list[j]
+        good_list = good_list.split(',')
+        for i in range(len(good_list)):
+            good_list[i] = float(good_list[i][2:-2])
+        block_data['POS'].iloc[j] = tuple(good_list)
+    block_data['MINO'] = block_data['HS_POP'] - \
+        (block_data['RRATIO']*block_data['HS_POP']).round()
+    block_data['enrollment']['420454051001'] = 0
+    block_data['is_school']['420454051001'] = False
+
+    # connecting disconnected districts. GerryChains has a more elegant method
+    G = nx.to_networkx_graph(block_data['borders'].to_dict())
+    # TODO: automate
+    G.add_edge('420293021012', '420293020002')
+    G.add_edge('420293021012', '420293021011')
+    G.add_edge('420293003012', '420293003022')
+    G.add_edge('420454033003', '420454016001')
+    G.add_edge('420454076006', '420454106024')
+    G.add_edge('420454017001', '420454020001')
+    return dist_dict,school_dict,block_data,G
+
+def signed_color(score):
+  if score >= 0: color = 'red'
+  else: color = 'green'
+  return colored('{:+7.2%}'.format(score),color)
+
+def make_map(new_dist,DATA_DIR):
+    bgs = []
+    colors1 = plt.cm.tab20_r(np.linspace(0., 1, 128))
+    colors2 = plt.cm.nipy_spectral_r(np.linspace(0, 1, 128))
+    colors = np.vstack((colors1, colors2))
+    mymap = mcolors.LinearSegmentedColormap.from_list(
+        'my_colormap', colors)
+    bg = gpd.read_file(
+        f'{DATA_DIR}/pa_groups /tl_2010_42_bg10.shp').set_index('GEOID10')
+    bg['new_dist'] = 0
+    for k, val in new_dist.items():
+        for v in val:
+            bg['new_dist'].loc[v] = int(k)
+            bgs.append(v)
+    bg['new_dist'] = bg['new_dist'].astype('category')
+    return bg.loc[bgs].plot(column='new_dist', categorical=True, cmap=mymap)
+
+
+def make_graph(new_dist,G,block_data):
+    display(block_data['POS'])
+    color_choice = ["#C0C0C0", '#808080', '#FF0000', '#800000', '#FFFF00', '#808000',
+                    '#00FF00', '#008000', '#00FFFF', '#008080', '#0000FF', '#000080', '#800080']
+    nx.set_node_attributes(G, 100, "size")
+    nx.set_node_attributes(G, '#9400D3', "color")
+    for i, school in enumerate(new_dist):
+        for u in new_dist[school]:
+            G.nodes[u]["color"] = color_choice[i % 13]
+        G.nodes[school]["color"] = "#FF00FF"
+        G.nodes[school]["size"] = 300
+    display(nx.get_node_attributes(G, "color").values())
+    return nx.draw(G, node_color=list(nx.get_node_attributes(G, "color").values()), pos=block_data['POS'].loc[list(G.nodes())], node_size=list(nx.get_node_attributes(G, 'size').values()))
--- gerry_tree/random_scores.py
+++ gerry_tree/random_scores.py
@@ -13,22 +13,26 @@ import matplotlib.colors as mcolors
 from pathlib import Path
 from typing import List, Dict
 
-def _read_file(path: str) -> Dict:
-      return json.load(Path(path).open())
+
+def read_file(path: str) -> Dict:
+    return json.load(Path(path).open())
+
 
 def random_spanning_tree(G):
-  T = G.copy()
-  rand = np.random.uniform(0, 1, T.size())
-  for i, edge in enumerate(T.edges()):
-      T.edges[edge]["weight"] = rand[i]
-  return nx.minimum_spanning_tree(T)
+    T = G.copy()
+    rand = np.random.uniform(0, 1, T.size())
+    for i, edge in enumerate(T.edges()):
+        T.edges[edge]["weight"] = rand[i]
+    return nx.minimum_spanning_tree(T)
 # setup steps to deal with messy files
+
+
 def cleanup(DATA_DIR):
-    block_data = pd.read_csv(f'{DATA_DIR}/small_final_attributes.csv')
+    block_data = pd.read_csv(f'{DATA_DIR}/small_final_attributes')
     # {block group : {school: distance in seconds from block group to school}}
-    dist_dict = _read_file(f'{DATA_DIR}/distance_data.txt')
+    dist_dict = read_file(f'{DATA_DIR}/distance_data.txt')
     # {school: [block groups in district]}
-    school_dict = _read_file(f'{DATA_DIR}/original_districts.txt')
+    school_dict = read_file(f'{DATA_DIR}/original_districts.txt')
     schoollist = school_dict.keys()
 
     # block_data is a table (pandas DataFrame) holding all the attributes of every block group and school (rows are block groups)
@@ -63,15 +67,18 @@ def cleanup(DATA_DIR):
     G.add_edge('420454033003', '420454016001')
     G.add_edge('420454076006', '420454106024')
     G.add_edge('420454017001', '420454020001')
-    return dist_dict,school_dict,block_data,G
+    return dist_dict, school_dict, block_data, G
 
-def redistrict(DATA_DIR,big_loop=1, steps=50, tree_loop=150, mino_coef=.5, buffer=0, random=False, enrollment_err=10/11, update_rate=5, animation='map'):
-    final_scores = []
-    dist_dict,school_dict,block_data,G = cleanup(DATA_DIR)
+
+def redistrict_random(DATA_DIR, samples=100, steps=50, buffer=0, enrollment_err=10/11):
+    dist_dict, original_school_dict, block_data, original_G = cleanup(DATA_DIR)
+    school_dict = original_school_dict
     schoollist = school_dict.keys()
     tot_mino = block_data["MINO"].sum()
     tot_pop = block_data['HS_POP'].sum()
     target_ratio = tot_mino/tot_pop
+
+    # score calculations (compactness is not implemented yet)
     def _calc_score(sch, district):
         pop = block_data['HS_POP'].loc[district].sum()
         mino = block_data['MINO'].loc[district].sum()
@@ -79,79 +86,229 @@ def redistrict(DATA_DIR,big_loop=1, steps=50, tree_loop=150, mino_coef=.5, buffe
         dem_score = (abs((mino/pop) - target_ratio))**2*pop*100
         # trav_score -> total drive time for all students (max ensures that when time < buffer, the value is not negative)
         trav_score = sum([max([dist_dict[x][sch]-buffer, 0]) *
-                      block_data['HS_POP'].loc[x] for x in district])
+                          block_data['HS_POP'].loc[x] for x in district])
         return (dem_score, trav_score)
-    district_scores = {dist: _calc_score(dist, school_dict[dist]) for dist in schoollist}
-    current_dem_score = np.round(sum([district_scores[u][0] for u in schoollist])/tot_pop,3)
-    current_trav_score = np.round(sum([district_scores[u][1] for u in schoollist])/tot_pop,3)
-    final_scores=[]
-    frames = 0
-    for k in tqdm(range(big_loop)):
-        trees = 0
-        valid = False
-        while trees < steps:
-            pair = list(zip(*choice(list(nx.quotient_graph(G,
-                                                        list(map(set, school_dict.values()))).edges()))))[0]
-            school_dict_r = {v: k for k, val in school_dict.items() for v in val}
-            schl1,schl2 = school_dict_r[pair[0]], school_dict_r[pair[1]]
+    # improvements is equivilent to steps, it counts the number of times we beat the current score (when we don't beat the score, the map doesn't change (assuming random == False))
+    district_scores, dem_scores, trav_scores = [], [], []
+    for i in tqdm(range(samples)):
+        school_dict = original_school_dict
+        G = original_G
+        improvements = 0
+        while improvements < steps:
+            schl1 = choice(list(school_dict))
+            neighbour_blocks = []
+            for i in school_dict[schl1]:
+                for block in G.neighbors(i):
+                    if block not in school_dict[schl1]:
+                        neighbour_blocks.append(block)
+            try:
+                block_choice = choice(neighbour_blocks)
+            except:
+                continue
+            for i in school_dict:
+                if block_choice in school_dict[i]:
+                    schl2 = i
+                    break
             district_pair = school_dict[schl1] + school_dict[schl2]
-            # initialize districts and scores to the current partitioning
-            best_district_1 = school_dict[schl1]
-            best_district_2 = school_dict[schl2]
+            # initialize districts and scores to the current partitionin
             subgraph_copy = nx.subgraph(G, district_pair).copy()
-            # counts the number of times we pick a tree without a good breaking (mostly for debugging)
+            # Have we gotten a better score?
+            improved = False
             # assigns random weights to edges then picks minimum tree this is a random spanning tree (hopefully)
-            T = random_spanning_tree(subgraph_copy)
-            
-            # get the shortest path between schools, we will cut one of these edges
-            school_path = list(nx.all_simple_paths(
-            T, source=schl1, target=schl2))[0]
+            for i in range(50):
+                T = random_spanning_tree(subgraph_copy)
 
-            # trash all the school_path edges and collect the connected components
-            # then we can go from one side of the path to the other and see which split gives balanced population
-            
-            T.remove_edges_from(nx.utils.pairwise(school_path))
-            comps = [list(nx.node_connected_component(T, i))
-                    for i in school_path]
-            split_pop = block_data['HS_POP'].loc[comps[0]].sum()
+                # get the shortest path between schools, we will cut one of these edges
+                school_path = list(nx.all_simple_paths(
+                    T, source=schl1, target=schl2))[0]
+
+                # trash all the school_path edges and collect the connected components
+                # then we can go from one side of the path to the other and see which split gives balanced population
+
+                T.remove_edges_from(nx.utils.pairwise(school_path))
+                comps = [list(nx.node_connected_component(T, i))
+                         for i in school_path]
+                split_pop = block_data['HS_POP'].loc[comps[0]].sum()
+                first_enroll = block_data['enrollment'].loc[schl1]
+                second_enroll = block_data['enrollment'].loc[schl2]
+                pair_pop = block_data['HS_POP'].loc[district_pair].sum()
+                # check enrollment capacities
+                for i in range(len(school_path)):
+                    # if we get past max. enrollment without a valid split, the tree cannot be broken
+                    if split_pop*enrollment_err > first_enroll:
+                        split = 0
+                        break
+                    if (pair_pop - split_pop)*enrollment_err < second_enroll and split_pop*enrollment_err < first_enroll:
+                        split = i+1
+                        break
+                    # add the population of the next component in the path
+                    split_pop += block_data['HS_POP'].loc[comps[i+1]].sum()
+                if split == 0:
+                    continue
+                # we have a split, now we make districts and check scores
+                district_1 = [y for x in [comps[i]
+                                          for i in range(split)] for y in x]
+                district_2 = [x for x in district_pair if x not in district_1]
+                school_dict[schl1] = district_1
+                school_dict[schl2] = district_2
+                improvements += 1
+                break
+        scoring = {dist: _calc_score(
+            dist, school_dict[dist]) for dist in schoollist}
+        district_scores.append(scoring)
+        dem_scores.append(
+            np.round(sum([scoring[u][0] for u in schoollist])/tot_pop, 3))
+        trav_scores.append(
+            np.round(sum([scoring[u][1] for u in schoollist])/tot_pop, 3))
+        return district_scores, dem_scores, trav_scores
+
+    def read_file(path: str) -> Dict:
+        return json.load(Path(path).open())
+
+    def random_spanning_tree(G):
+        T = G.copy()
+        rand = np.random.uniform(0, 1, T.size())
+        for i, edge in enumerate(T.edges()):
+            T.edges[edge]["weight"] = rand[i]
+            return nx.minimum_spanning_tree(T)
+    # setup steps to deal with messy files
+
+
+def cleanup(DATA_DIR):
+    block_data = pd.read_csv(f'{DATA_DIR}/small_final_attributes.csv')
+    # {block group : {school: distance in seconds from block group to school}}
+    dist_dict = read_file(f'{DATA_DIR}/distance_data.txt')
+    # {school: [block groups in district]}
+    school_dict = read_file(f'{DATA_DIR}/original_districts.txt')
+    schoollist = school_dict.keys()
+
+    # block_data is a table (pandas DataFrame) holding all the attributes of every block group and school (rows are block groups)
+    # dealing with the messiness of pandas read_csv with list/tuple entries
+    block_data['GEOID10'] = block_data['GEOID10'].astype('str')
+    block_data = block_data.set_index('GEOID10')
+    for j in range(len(block_data)):
+        string_list = block_data['borders'].iloc[j].split(',')[1:-1]
+        good_list = []
+        for i in range(len(string_list)):
+            if string_list[i][2:-1] in block_data.index:
+                good_list.append(string_list[i][2:-1])
+        block_data['borders'].iloc[j] = good_list
+    string_list = list(block_data['POS'])
+    for j in range(len(block_data)):
+        good_list = string_list[j]
+        good_list = good_list.split(',')
+        for i in range(len(good_list)):
+            good_list[i] = float(good_list[i][2:-2])
+        block_data['POS'].iloc[j] = tuple(good_list)
+    block_data['MINO'] = block_data['HS_POP'] - \
+        (block_data['RRATIO']*block_data['HS_POP']).round()
+    block_data['enrollment']['420454051001'] = 0
+    block_data['is_school']['420454051001'] = False
+
+    # connecting disconnected districts. GerryChains has a more elegant method
+    G = nx.to_networkx_graph(block_data['borders'].to_dict())
+    # TODO: automate
+    G.add_edge('420293021012', '420293020002')
+    G.add_edge('420293021012', '420293021011')
+    G.add_edge('420293003012', '420293003022')
+    G.add_edge('420454033003', '420454016001')
+    G.add_edge('420454076006', '420454106024')
+    G.add_edge('420454017001', '420454020001')
+    return dist_dict, school_dict, block_data, G
+
+
+def redistrict_random(DATA_DIR, samples=100, steps=50, buffer=0, enrollment_err=10/11):
+    dist_dict, original_school_dict, block_data, original_G = cleanup(DATA_DIR)
+    school_dict = original_school_dict
+    schoollist = school_dict.keys()
+    tot_mino = block_data["MINO"].sum()
+    tot_pop = block_data['HS_POP'].sum()
+    target_ratio = tot_mino/tot_pop
+
+    # score calculations (compactness is not implemented yet)
+    def _calc_score(sch, district):
+        pop = block_data['HS_POP'].loc[district].sum()
+        mino = block_data['MINO'].loc[district].sum()
+        # dem_score -> squared difference between minority ratio of district and overall minority ratio * district population
+        dem_score = (abs((mino/pop) - target_ratio))**2*pop*100
+        # trav_score -> total drive time for all students (max ensures that when time < buffer, the value is not negative)
+        trav_score = sum([max([dist_dict[x][sch]-buffer, 0]) *
+                          block_data['HS_POP'].loc[x] for x in district])
+        return (dem_score, trav_score)
+    # improvements is equivilent to steps, it counts the number of times we beat the current score (when we don't beat the score, the map doesn't change (assuming random == False))
+    district_scores, dem_scores, trav_scores = [], [], []
+    for i in tqdm(range(samples)):
+        import ipdb; ipdb.set_trace()
+        school_dict = original_school_dict
+        G = original_G
+        improvements = 0
+        while improvements < steps:
+            schl1 = choice(list(school_dict))
+            neighbour_blocks = []
+            for i in school_dict[schl1]:
+                for block in G.neighbors(i):
+                    if block not in school_dict[schl1]:
+                        neighbour_blocks.append(block)
+            try:
+                block_choice = choice(neighbour_blocks)
+            except:
+                continue
+            for i in school_dict:
+                if block_choice in school_dict[i]:
+                    schl2 = i
+                    break
+            district_pair = school_dict[schl1] + school_dict[schl2]
+            # initialize districts and scores to the current partitionin
+            subgraph_copy = nx.subgraph(G, district_pair).copy()
+            # Have we gotten a better score?
+            improved = False
+            # assigns random weights to edges then picks minimum tree this is a random spanning tree (hopefully)
             first_enroll = block_data['enrollment'].loc[schl1]
             second_enroll = block_data['enrollment'].loc[schl2]
             pair_pop = block_data['HS_POP'].loc[district_pair].sum()
-            # check enrollment capacities
-            for j in range(len(school_path)):
-                if split_pop*enrollment_err > first_enroll:
-                    split = 0
-                    break
-                if (pair_pop - split_pop)*enrollment_err < second_enroll:
-                    split = j+1
-                    valid = True
-                    trees +=1
-                    break
-                split_pop += block_data['HS_POP'].loc[comps[j+1]].sum()
-            if not valid:
-                continue
-            district_1 = [y for x in [comps[i]
-                                    for i in range(split)] for y in x]
-            district_2 = [x for x in district_pair if x not in district_1]
-            dem_score1, trav_score1 = _calc_score(schl1, district_1)
-            dem_score2, trav_score2 = _calc_score(schl2, district_2)
-            all_scores = ((dem_score1, trav_score1),(dem_score2, trav_score2))
-            district_scores[schl1] = all_scores[0]
-            district_scores[schl2] = all_scores[1]
-            school_dict[schl1] = district_1
-            school_dict[schl2] = district_2
+            for i in range(50):
+                T = random_spanning_tree(subgraph_copy)
 
+                # get the shortest path between schools, we will cut one of these edges
+                school_path = list(nx.all_simple_paths(
+                    T, source=schl1, target=schl2))[0]
 
-        current_dem_score = np.round(sum([district_scores[u][0] for u in schoollist])/tot_pop,6)
-        current_trav_score = np.round(sum([district_scores[u][1] for u in schoollist])/tot_pop,6)
-        final_scores.append((district_scores, current_dem_score, current_trav_score))
-    return final_scores
+                # trash all the school_path edges and collect the connected components
+                # then we can go from one side of the path to the other and see which split gives balanced population
+
+                T.remove_edges_from(nx.utils.pairwise(school_path))
+                comps = [list(nx.node_connected_component(T, i)) for i in school_path]
+                split_pop = block_data['HS_POP'].loc[comps[0]].sum()
+                # check enrollment capacities
+                for i in range(len(school_path)):
+                    # if we get past max. enrollment without a valid split, the tree cannot be broken
+                    if split_pop*enrollment_err > first_enroll:
+                        split = 0
+                        break
+                    if (pair_pop - split_pop)*enrollment_err < second_enroll and split_pop*enrollment_err < first_enroll:
+                        split = i+1
+                        break
+                    # add the population of the next component in the path
+                    split_pop += block_data['HS_POP'].loc[comps[i+1]].sum()
+                if split == 0:
+                    continue
+                district_1 =  [y for x in [comps[i] for i in range(split)] for y in x]
+                school_dict[schl1] = district_1
+                school_dict[schl2] = [x for x in district_pair if x not in district_1]
+                improvements += 1
+                break
+        scoring = {dist: _calc_score(
+            dist, school_dict[dist]) for dist in schoollist}
+        district_scores.append(scoring)
+        dem_scores.append(
+            np.round(sum([scoring[u][0] for u in schoollist])/tot_pop, 3))
+        trav_scores.append(
+            np.round(sum([scoring[u][1] for u in schoollist])/tot_pop, 3))
+    return district_scores, dem_scores, trav_scores
 data_dir = '/Users/zoeshleifer/promys2020/lab/Gerrymandering2-promys2020/gerry_tree'
-
+results1k = redistrict_random(data_dir,steps=30,samples=1000)
 def pickle_save(obj, path):
     with open(path, "wb") as f:
         return pickle.dump(obj, f)
 
-
-new_results = (redistrict(data_dir,big_loop=1000,steps=30,mino_coef=1,tree_loop=1,random=True))
-pickle_save(new_results,'redistrict_random_results30.pkl')
+pickle_save(results1k,'random_results1k.pkl')
